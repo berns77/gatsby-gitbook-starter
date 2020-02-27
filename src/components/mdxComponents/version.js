991d7851be0ef4bc1d469c90efbbd76d0a5f1c3d
Alex Berns <alex.berns@renaissance.com>
2020-02-27 16:12:55 -0600

    Update version.js

--- src/components/mdxComponents/version.js
+++ src/components/mdxComponents/version.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from 'react';
 export default ({ url = 'https://staging.myon.com/' }) => {
     const [version, setVersion] = useState('unknown');
 
-    useEffect(async () => {
-        const result = await fetch(url);
-
-        console.log(result);
-
-        setVersion('1.0');
-    });
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(url);
+            setVersion(result.data);
+        };
+        fetchData();
+    }, []);
 
     return (
         <div>
