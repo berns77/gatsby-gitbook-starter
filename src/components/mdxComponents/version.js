c6239e5808d1746f3df9e3cf8732c3e8131000de
Alex Berns <alex.berns@renaissance.com>
2020-03-09 14:53:18 -0500

    update

--- src/components/mdxComponents/version.js
+++ src/components/mdxComponents/version.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-export default ({ url = 'https://staging.myon.com/', version = 2 }) => {
-    const stage = process.env;
-
-    console.log(stage);
-
+export default ({ url = 'https://staging.myon.com/', env }) => {
     return (
         <div>
-            version {url} {version}
-            <pre>{JSON.stringify(stage, null, 2)}</pre>
+            version {url}
+            <pre>{JSON.stringify(env, null, 2)}</pre>
         </div>
     );
 };
