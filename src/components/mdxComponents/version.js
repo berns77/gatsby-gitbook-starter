c25e9eca5de81afb432871319a67e18de6cd1358
Alex Berns <alex.berns@renaissance.com>
2020-03-09 15:21:31 -0500

    Update version.js

--- src/components/mdxComponents/version.js
+++ src/components/mdxComponents/version.js
@@ -3,11 +3,12 @@ import React from 'react';
 export default ({ url = 'https://staging.myon.com/', env = {} }) => {
     const { INCOMING_HOOK_BODY } = env;
     const str = decodeURI(INCOMING_HOOK_BODY);
-    console.log(str);
+    const payload=JSON.parse(str);
+    console.log(payload);
     return (
         <div>
             version {url}
-            <pre>{JSON.stringify(str, null, 2)}</pre>
+            <pre>{JSON.stringify(payload, null, 2)}</pre>
         </div>
     );
 };
